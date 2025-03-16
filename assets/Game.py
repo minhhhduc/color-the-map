2b65f78d28c3de906aae38817fc5ef0687285daa
minhhhduc <dominhduc02102005@gmail.com>
2025-03-17 00:56:50 +0700

    add screen game

---
+++ assets/Game.py
@@ -0,0 +1,12 @@
+import pygame
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("color the map")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
